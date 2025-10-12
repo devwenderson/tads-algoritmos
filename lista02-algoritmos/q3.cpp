97a55e634f455c912a63f886c86505aad13c5415
devwenderson <wend.devdesigner@gmail.com>
2025-10-12 12:47:38 -0300

    wip: lista02-algoritmos

---
+++ lista02-algoritmos/q3.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int maior_diferenca(int a[], int tam) {
+    int maior = 0;
+
+    for (int i = 1; i < tam; ++i) {
+        if (a[i-1] + a[i] > maior) {
+            maior = a[i-1] + a[i];
+        };
+    }
+
+    return maior;
+}
+
+int main() {
+    int tam;
+    cin >> tam;
+    int a[tam];
+    for (int i = 0; i < tam; ++i) cin >> a[i];
+    int maior = maior_diferenca(a, tam);
+    cout << maior << endl; 
+    return 0;
+}
